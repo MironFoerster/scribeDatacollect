032810fb3667822a88af6fbffc03477ea37f2616
MironFoerster <miron.foerster@gmx.de>
2022-02-14 19:52:48 +0100

    basic structure setup

--- scribedata/split/models.py
+++ scribedata/split/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class PersonSplit(models.Model):
+    name = models.CharField(max_length=50)
+    current_index = models.IntegerField()
+    rejected_index_string = models.CharField(max_length=500)
--- scribedata/write/models.py
+++ scribedata/write/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class PersonWrite(models.Model):
+    name = models.CharField(max_length=50)
+    current_index = models.IntegerField()
