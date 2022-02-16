3d8b8c02a5c220b90c8adbc9dbc58d4ee5f14637
MironFoerster <miron.foerster@gmx.de>
2022-02-16 20:06:21 +0100

    create custom django-admin management commands

---
+++ scribedata/intro/management/commands/setupusers.py
@@ -0,0 +1,25 @@
+import csv
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'resetting and creating all Users from csv file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('users_fp', nargs=1)
+
+    def handle(self, *args, **options):
+        users_fp = options['users_fp']
+        # delete all existing users
+        User.objects.all().delete()
+
+        try:
+            with open(users_fp) as users_file:
+                users_reader = csv.reader(users_file)
+                for username in users_reader:
+                    User.objects.create_user(username[0])
+        except FileNotFoundError:
+            raise CommandError('File "%s" does not exist!' % users_fp)
+
+        self.stdout.write(self.style.SUCCESS('Successfully initialized Users from "%s"!' % users_fp))
---
+++ scribedata/write/management/commands/setupwrite.py
@@ -0,0 +1,50 @@
+import csv
+import os
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from write.models import PersonWrite
+
+class Command(BaseCommand):
+    help = 'resetting and creating all write-data infrastructure'
+
+    def add_arguments(self, parser):
+        parser.add_argument('tasks_fp', nargs=1)
+
+    def handle(self, *args, **options):
+        # delete all existing PersonWrites
+        PersonWrite.objects.all().delete()
+
+        tasks_fp = options['tasks_fp']
+
+        # count all tasks and find out tasks per person
+        forcount_tasks_reader = csv.reader(tasks_fp, delimiter=';')
+        num_tasks = sum(1 for task in forcount_tasks_reader)-1  # minus header row
+        num_users = User.objects.all().count()
+        tasks_per_person = num_tasks // num_users
+        num_dropped_tasks = num_tasks % num_users
+
+        self.stdout.write('There are %d persons!' % num_users)
+        self.stdout.write('There are %d write tasks!' % num_tasks)
+        self.stdout.write('Every person will get %d write tasks!' % tasks_per_person)
+        self.stdout.write('%d write tasks will get dropped!' % num_dropped_tasks)
+
+        full_tasks_reader = csv.reader(tasks_fp, delimiter=';')
+        next(full_tasks_reader)  # skip header
+
+        try:
+            for user in User.objects.all():
+                person_dp = os.path.join(settings.BASE_DIR, 'static/csv/work/', user.username)
+                os.makedirs(person_dp, exist_ok=True)
+
+                with open(os.path.join(person_dp, 'write_tasks.csv'), 'w') as person_tasks_file:
+                    person_tasks_writer = csv.writer(person_tasks_file, delimiter=';')
+
+                    person_tasks_writer.writerow(['data'])  # write header
+                    for i in range(tasks_per_person):
+                        person_tasks_writer.writerow(next(full_tasks_reader))
+
+        except:
+            raise CommandError('There was a Problem... and this exception handling is huge garbage...')
+
+        self.stdout.write(self.style.SUCCESS('Successfully initialized write-data infrastructure!'))
