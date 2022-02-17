e00378f031faacaacb3442df6662d45ba6dcc892
MironFoerster <miron.foerster@gmx.de>
2022-02-17 16:23:26 +0100

    build better data processing in views

--- scribedata/split/views.py
+++ scribedata/split/views.py
@@ -1,43 +1,45 @@
-import csv
 import json
-
+import csv
+import os
+from .models import PersonSplit
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import PersonSplit
-from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 def index(request):
     context = {}
     return render(request, 'split.html', context=context)
 
+@login_required(login_url='/login/')
 def data(request):
-    try:
-        p = PersonSplit.objects.get(name=request.user.username)
-    except ObjectDoesNotExist:
-        return JsonResponse({'err': 'ObjectDoesNotExist', 'msg': 'Du bist nicht eingeloggt!'})
+    p = PersonSplit.objects.get(name=request.user.username)
 
-    # process submit
+    # PROCESS SUBMIT
     submit = json.loads(request.body)
-    #check if submit is valid
+    # check if submit is containing data or is only requesting a task
     if all(key in submit.keys() for key in ['index', 'data', 'reject']):
+        # if submit corresponds to the current task
         if submit['index'] == p.current_task:
             p.current_task += 1
+            p.save()
+
             if not submit['reject']:
-                with open('../static/csv/split_submits.csv') as f:
-                    fieldnames = ['data', 'person']
+                with open(os.path.join('../static/csv/split/', p.name, 'submits.csv'), 'a') as f:
+                    fieldnames = ['strokes', 'text', 'person']
                     submits_writer = csv.DictWriter(f, fieldnames=fieldnames)
-                    submits_writer.writerow({'data': submit['data'], 'person': p.name})
+                    for row in submit['data']:
+                        submits_writer.writerow({'strokes': row['strokes'], 'text': row['text'], 'person': row['person']})
                 msg = 'Erfolgreich in den Datensatz eingetragen!'
             else:
                 msg = 'Erfolgreich aus dem Datensatz entfernt!'
         else:
             msg = 'Dieser Eintrag war schon vorhanden!'
+    else:  # only requesting a task
+        msg = 'Es wurden keine Daten gesendet!'
 
-
-    # send next task
-    with open('../static/csv/setup/split_tasks.csv') as f:
+    # SEND NEXT TASK
+    with open(os.path.join('../static/csv/write/', p.name, 'tasks.csv'), 'r') as f:
         tasks_reader = csv.DictReader(f)
         # skip csv header
         next(tasks_reader)
@@ -45,11 +47,12 @@ def data(request):
         for i in range(p.current_task):
             next(tasks_reader)
         # read current task
-        current_task_data = next(tasks_reader)
+        task = next(tasks_reader)
+        task_data = {'strokes': task['strokes'], 'text': task['text'], 'person': task['person']}
 
     response = {
         'id': p.current_task,
-        'data': current_task_data,
+        'data': task_data,
         'msg': msg
     }
     return JsonResponse(response)
--- scribedata/write/views.py
+++ scribedata/write/views.py
@@ -1,37 +1,42 @@
 import json
+import csv
+import os
 from .models import PersonWrite
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def index(request):
     context = {}
     return render(request, 'write.html', context=context)
 
+@login_required(login_url='/login/')
 def data(request):
-    try:
-        p = PersonWrite.objects.get(name=request.user.username)
-    except ObjectDoesNotExist:
-        return JsonResponse({'err': 'ObjectDoesNotExist', 'msg': 'Du bist nicht eingeloggt!'})
+    p = PersonWrite.objects.get(name=request.user.username)
 
-    # process submit
+    # PROCESS SUBMIT
     submit = json.loads(request.body)
-    # check if submit is valid
+    # check if submit is containing data or is only requesting a task
     if all(key in submit.keys() for key in ['index', 'data']):
+        # if submit corresponds to the current task
         if submit['index'] == p.current_task:
             p.current_task += 1
+            p.save()
 
-            with open('../static/csv/write_submits.csv') as f:
-                fieldnames = ['data', 'person']
+            with open(os.path.join('../static/csv/write/', p.name, 'submits.csv'), 'a') as f:
+                fieldnames = ['strokes', 'text', 'person']
                 submits_writer = csv.DictWriter(f, fieldnames=fieldnames)
-                submits_writer.writerow({'data': submit['data'], 'person': p.name})
+                for row in submit['data']:
+                    submits_writer.writerow({'strokes': row['strokes'], 'text': row['text'], 'person': row['person']})
             msg = 'Erfolgreich in den Datensatz eingetragen!'
         else:
             msg = 'Dieser Eintrag war schon vorhanden!'
+    else:  # only requesting a task
+        msg = 'Es wurden keine Daten gesendet!'
 
-    # send next task
-    with open('../static/csv/setup/write_tasks.csv') as f:
+    # SEND NEXT TASK
+    with open(os.path.join('../static/csv/write/', p.name, 'tasks.csv'), 'r') as f:
         tasks_reader = csv.DictReader(f)
         # skip csv header
         next(tasks_reader)
@@ -39,11 +44,12 @@ def data(request):
         for i in range(p.current_task):
             next(tasks_reader)
         # read current task
-        current_task_data = next(tasks_reader)
+        task = next(tasks_reader)
+        task_data = {'text': task['text'], 'person': p.name}
 
     response = {
         'id': p.current_task,
-        'data': current_task_data,
+        'data': task_data,
         'msg': msg
     }
     return JsonResponse(response)
