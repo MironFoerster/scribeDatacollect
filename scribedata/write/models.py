cd2b35f8010edda7ee6c7059587b1f347d0585d5
MironFoerster <miron.foerster@gmx.de>
2022-02-23 20:42:06 +0100

    personwrite current_index to current_task

--- scribedata/write/models.py
+++ scribedata/write/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class PersonWrite(models.Model):
     name = models.CharField(max_length=50)
-    current_index = models.IntegerField(default=0)
+    current_task = models.IntegerField(default=0)
